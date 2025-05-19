981cb7a281aee62840ad33e93a1b0a6eda72c2ab
himanshuDev <himanshu.nikhil.gupta@gmail.com>
2025-05-19 17:58:25 +0530

    Update test.js
    funcation code

--- test.js
+++ test.js
@@ -1,3 +1,30 @@
 function add(numbers) {
+   if (!numbers) return 0;
+
+  let delimiter = /,|\n/; // default delimiters: comma or newline
+  let numberString = numbers;
+
+  // Custom delimiter support
+  if (numbers.startsWith("//")) {
+    const delimiterMatch = numbers.match(/^\/\/(.+)\n/);
+    if (delimiterMatch) {
+      let customDelimiter = delimiterMatch[1];
+
+      // Support delimiters in brackets: //[***]\n1***2***3
+      if (customDelimiter.startsWith("[") && customDelimiter.endsWith("]")) {
+        const delimiters = customDelimiter
+          .match(/\[([^\]]+)\]/g)
+          .map(d => d.slice(1, -1).replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'));
+        delimiter = new RegExp(delimiters.join("|"), "g");
+      } else {
+        // Single character delimiter
+        delimiter = new RegExp(customDelimiter.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'));
+      }
+
+      numberString = numbers.slice(delimiterMatch[0].length);
+    }
+  }
+
+  return numberString; 
   
 }
